2d92d49e7d17149b723d00519e598d2ba668c4b1
Dmitri <rubanovdp@gmail.com>
2023-04-04 11:22:36 +0300

    add fixes to CW 03.04

--- src/telran/employees/application/controler/CompanyClientAppl.java
+++ src/telran/employees/application/controler/CompanyClientAppl.java
@@ -3,7 +3,6 @@ package telran.employees.application.controler;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.InputStream;
 import java.lang.reflect.Constructor;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -12,21 +11,24 @@ import java.util.Properties;
 import telran.employees.Company;
 import telran.employees.net.NetworkCompany;
 import telran.net.NetworkClient;
-import telran.net.TcpClient;
-import telran.sportsmen.Sportsman;
 import telran.view.Item;
 import telran.view.Menu;
 import telran.view.StandartInputOutput;
 
 public class CompanyClientAppl {
+	private static final String TRANSPORT = "transport";
+	private static final String DEPARTMENTS = "departments";
+	private static final String CLIENT = "Client";
+	private static final String HOST_NAME = "hostName";
+	private static final String PORT = "port";
 	private static final String PACKAGE_NAME = "telran.net.";
 	private static Properties props = new Properties();
 
 	public static void main(String[] args) throws Exception {
 		StandartInputOutput io = new StandartInputOutput();
-		NetworkClient client = getClient("transport", args);
+		NetworkClient client = getClient(TRANSPORT, args);
 		Company company = new NetworkCompany(client);
-		String[] departments = getDepartments("departments", args);
+		String[] departments = getDepartments(DEPARTMENTS, args);
 		Item[] companyItems = CompanyControllerItems.getCompanyItems(company, departments);
 		ArrayList<Item> items = new ArrayList<>(Arrays.asList(companyItems));
 		items.add(GetExitItem(company));
@@ -57,12 +59,12 @@ public class CompanyClientAppl {
 	private static NetworkClient getClient(String key, String[] args) {
 		NetworkClient res = null;
 		String transport = getProperties(key, args);
-		int port = Integer.parseInt(getProperties("port", args));
-		String host = getProperties("hostName", args);
+		int port = Integer.parseInt(getProperties(PORT, args));
+		String host = getProperties(HOST_NAME, args);
 		if (transport != null) {
 			try {
 				Class<NetworkClient> clientClass = (Class<NetworkClient>) Class
-						.forName(PACKAGE_NAME + transport + "Client");
+						.forName(PACKAGE_NAME + transport + CLIENT);
 				Constructor<NetworkClient> constructor = clientClass.getConstructor(String.class,
 						int.class);
 				res = constructor.newInstance(host, port);
@@ -79,9 +81,10 @@ public class CompanyClientAppl {
 			props.load(new FileInputStream(args[0]));
 			res = props.getProperty(key);
 		} catch (FileNotFoundException e) {
-			throw new RuntimeException("Wrong file name");
-		} catch (IOException e) {
+			System.out.println("Wrong file name");
 			e.printStackTrace();
+		} catch (Exception e) {
+			System.out.println("Some problems with configurations params");;
 		}
 		return res;
 	}
